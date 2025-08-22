8a737c0bb47b761d5848f2b6d5342768234f736f
Martan <martina.merglova@gmail.com>
2025-08-22 13:16:08 +0200

    added all the files needed

---
+++ skripty.js
@@ -0,0 +1,18 @@
+function ahoj(jmeno)
+ {alert("Ahoj, "+jmeno)}
+
+function jejda(nehoda) 
+{alert("Jejda, " +nehoda)}
+
+function priklad(zadani) 
+{alert("příklad: " +zadani)}
+
+function vysledekobsah(vysledek) 
+{alert("výsledek příkladu obsah: " +vysledek)}
+
+function credit(kdoco) 
+{alert("dobrý nápad: " + kdoco)}
+
+function hodnocení(body) 
+{alert("Děkuji za tvé ohodnocení: " +body)}
+
